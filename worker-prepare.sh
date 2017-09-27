fc3c4f7437fca267fc67838516c2d3329649744e
davidkhala <david-khala@hotmail.com>
2017-09-27 09:33:21 +0800

    fix

--- worker-prepare.sh
+++ worker-prepare.sh
@@ -6,9 +6,9 @@ thisIP=$(./common/docker/utils/swarm.sh getNodeIP $mainNodeID)
 
 CONFIGTX_nfs="/home/david/Documents/nfs/CONFIGTX"
 MSPROOT_nfs="/home/david/Documents/nfs/MSPROOT"
-CONFIGTX_DIR=$(./common/docker/utils/swarm.sh getNodeLabels | jq -r ".CONFIGTX")
+CONFIGTX_DIR=$(./common/docker/utils/swarm.sh getNodeLabels $mainNodeID | jq -r ".CONFIGTX")
 
-MSPROOT_DIR=$(./common/docker/utils/swarm.sh getNodeLabels | jq -r ".MSPROOT")
+MSPROOT_DIR=$(./common/docker/utils/swarm.sh getNodeLabels $mainNodeID | jq -r ".MSPROOT")
 if [ ! "$MSPROOT_DIR"=="null" ]; then
 	./common/docker/utils/nfs.sh add $thisIP $MSPROOT_DIR $MSPROOT_nfs
 fi
