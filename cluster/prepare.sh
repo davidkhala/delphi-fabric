04aa96982135e3a7dbe17e87e64337127ea09b93
davidkhala <david-khala@hotmail.com>
2017-09-27 11:13:47 +0800

    fix path

--- cluster/prepare.sh
+++ cluster/prepare.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 CURRENT="$(dirname $(readlink -f $BASH_SOURCE))"
+utilsDir="$(dirname $CURRENT)/common/docker/utils"
 mainNodeID="ubuntu"
 CONFIG_DIR="$(dirname $CURRENT)/config/"
 CONFIG_JSON="$CONFIG_DIR/orgs.json"
@@ -7,24 +8,25 @@ COMPANY="delphi"
 volumesConfig=$(jq -r ".$COMPANY.docker.volumes" $CONFIG_JSON)
 
 
-thisIP=$(../common/docker/utils/swarm.sh getNodeIP $mainNodeID)
+thisIP=$($utilsDir/swarm.sh getNodeIP $mainNodeID)
 
 CONFIGTX_nfs="/home/david/Documents/nfs/CONFIGTX"
 MSPROOT_nfs="/home/david/Documents/nfs/MSPROOT"
-CONFIGTX_DIR=$(../common/docker/utils/swarm.sh getNodeLabels $mainNodeID | jq -r ".CONFIGTX")
+CONFIGTX_DIR=$($utilsDir/swarm.sh getNodeLabels $mainNodeID | jq -r ".CONFIGTX")
 
-MSPROOT_DIR=$(../common/docker/utils/swarm.sh getNodeLabels $mainNodeID | jq -r ".MSPROOT")
+MSPROOT_DIR=$($utilsDir/swarm.sh getNodeLabels $mainNodeID | jq -r ".MSPROOT")
 if [ ! "$MSPROOT_DIR" == "null" ]; then
-	../common/docker/utils/nfs.sh add $thisIP $MSPROOT_DIR $MSPROOT_nfs
+    $utilsDir/nfs.sh add $thisIP $MSPROOT_DIR $MSPROOT_nfs
 else
- exit 1
+    exit 1
 fi
 if [ ! "$CONFIGTX_DIR" == "null" ]; then
-	../common/docker/utils/nfs.sh add $thisIP $CONFIGTX_DIR $CONFIGTX_nfs
-	else exit 1
+    $utilsDir/nfs.sh add $thisIP $CONFIGTX_DIR $CONFIGTX_nfs
+else
+    exit 1
 fi
 CONFIGTX_swarm=$(echo $volumesConfig | jq -r ".CONFIGTX.swarm")
 MSPROOT_swarm=$(echo $volumesConfig | jq -r ".MSPROOT.swarm")
-../common/docker/utils/volume.sh createLocal $CONFIGTX_swarm $MSPROOT_DIR
-../common/docker/utils/volume.sh createLocal $MSPROOT_swarm $CONFIGTX_DIR
+$utilsDir/volume.sh createLocal $CONFIGTX_swarm $CONFIGTX_DIR
+$utilsDir/volume.sh createLocal $MSPROOT_swarm $MSPROOT_DIR
 
