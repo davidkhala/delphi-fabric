f6d90a148ac997853c2a96e35ac8429c4696c594
Yuxiang LIU <david-khala@hotmail.com>
2017-11-20 11:11:48 +0800

    to support CN

--- install.sh
+++ install.sh
@@ -20,6 +20,14 @@ function yamlGolang() {
 	ln -s $(which yaml) /usr/local/bin/yaml
 
 }
+function cn(){
+    if [ ! -f "$CURRENT/common/install.sh" ]; then
+		gitSync
+	fi
+    $CURRENT/common/install.sh cn
+    apt -qq install -y moreutils
+	npm install
+}
 function gitSync(){
     git pull
     git submodule update --init --recursive
