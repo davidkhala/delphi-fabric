bc84711851c3282a174936cac65936144fd71011
davidliu <david-khala@hotmail.com>
2018-03-24 21:26:32 +0800

    back up

--- cluster/managerNode/prepare.sh
+++ cluster/managerNode/prepare.sh
@@ -9,18 +9,14 @@ CONFIG_DIR="$root/config"
 
 SWARM_CONFIG="$root/swarm/swarm.json"
 
-swarmServerIP="$1"
 nodeHostName=$2 # new hostname
 
-if [ -z $swarmServerIP ];then
-    echo empty swarmServerIP [1st parameter]
-    exit 1
-fi
 
+swarmServerIP=$(jq ".swarmServer.url" $SWARM_CONFIG)
 swarmServerPort=$(jq ".swarmServer.port" $SWARM_CONFIG)
 swarmBaseUrl=${swarmServerIP}:${swarmServerPort}
 
-if ! curl -s http://$swarmBaseUrl/ ;then
+if ! curl -s $swarmBaseUrl/ ;then
     echo no response from swarmServer $swarmBaseUrl
     exit 1
 else echo
@@ -30,7 +26,7 @@ if [ -n "$nodeHostName" ];then
     $ubuntuDir/hostname.sh change $nodeHostName
 fi
 
-CONFIG_JSON=$(curl -s http://$swarmBaseUrl/config/orgs)
+CONFIG_JSON=$(curl -s $swarmBaseUrl/config/orgs)
 fabricTag=$(echo $CONFIG_JSON| jq -r ".docker.fabricTag")
 $root/cluster/prepare.sh pull $fabricTag
 
@@ -43,20 +39,22 @@ MSPROOT_volumeName="MSPROOT_swarm"
 mkdir -p $CONFIGTX_nfs
 mkdir -p $MSPROOT_nfs
 
-leaderInfo=$(curl -s http://${swarmBaseUrl}/leader)
+leaderInfo=$(curl -s ${swarmBaseUrl}/leader)
 
 joinToken=$(echo $leaderInfo| jq -r ".managerToken")
-$joinToken # try to join
+if ! $utilsDir/swarm.sh belongTo "$joinToken" ;then
+    $joinToken # try to join
+fi
 
-CONFIGTX_DIR=$(curl -X POST http://${swarmBaseUrl}/volume/get -d '{"key":"CONFIGTX"}' -H "Content-Type: application/json")
+CONFIGTX_DIR=$(eval echo $(curl -s -X POST ${swarmBaseUrl}/volume/get -d '{"key":"CONFIGTX"}' -H "Content-Type: application/json"))
 
-MSPROOT_DIR=$(curl -X POST http://${swarmBaseUrl}/volume/get -d '{"key":"MSPROOT"}' -H "Content-Type: application/json")
+MSPROOT_DIR=$(eval echo $(curl -s -X POST ${swarmBaseUrl}/volume/get -d '{"key":"MSPROOT"}' -H "Content-Type: application/json"))
 
 mainNodeIP=$(echo $leaderInfo | jq -r ".ip")
-echo mountClient[MSPROOT] $MSPROOT_nfs $mainNodeIP $MSPROOT_DIR
-$ubuntuDir/nfs.sh mountClient $MSPROOT_nfs $mainNodeIP $MSPROOT_DIR
+echo mountClient[MSPROOT] $MSPROOT_nfs $mainNodeIP ${MSPROOT_DIR}
+sudo $ubuntuDir/nfs.sh mountClient $MSPROOT_nfs $mainNodeIP $MSPROOT_DIR
 echo mountClient[CONFIGTX] $CONFIGTX_nfs $mainNodeIP $CONFIGTX_DIR
-$ubuntuDir/nfs.sh mountClient $CONFIGTX_nfs $mainNodeIP $CONFIGTX_DIR
+sudo $ubuntuDir/nfs.sh mountClient $CONFIGTX_nfs $mainNodeIP $CONFIGTX_DIR
 
 $root/cluster/clean.sh
 docker volume prune --force
