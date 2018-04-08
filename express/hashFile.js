06b669aec9243f73559bc5ee35c9ac66c00fc106
davidliu <david-khala@hotmail.com>
2018-04-08 17:07:36 +0800

    in progress: new hash function

--- express/hashFile.js
+++ express/hashFile.js
@@ -19,7 +19,7 @@ const errorHandle = (err, res) => {
 	const errorCodeMap = require('./errorCodeMap.json');
 
 	let status = 500;
-	for (let errorMessage in errorCodeMap) {
+	for (const errorMessage in errorCodeMap) {
 		if (err.toString().includes(errorMessage)) {
 			status = errorCodeMap[errorMessage];
 			break;
@@ -28,32 +28,73 @@ const errorHandle = (err, res) => {
 	res.status(status).json({error: err.toString()});
 
 };
+router.use((req, res, next) => {
+	const user = req.body;
+	if (!user) return errorHandle(`user is ${user}`, res);
+	const {username, password, token} = user;
+	const oauthClient = require('./oauth2Client').passwordGrant;
+
+	if (!token) {
+		if (!username || !password) {
+			return errorHandle(`missing user identity  ${user}`, res);
+		}
+		oauthClient.getToken({username, password})
+			.then(accessToken => {
+				res.locals.accessToken = accessToken.accessToken.accessToken;
+				next();
+				return Promise.resolve(accessToken.accessToken.accessToken);
+			});
+	} else {
+		//TODO request TK platform to authenticate token
+		oauthClient.verify(token).then(result => {
+			if (result) {
+				res.locals.accessToken = token;
+				next();
+			}
+		});
+
+
+	}
+});
+const Request = require('request');
 router.post('/write', cache.array('files'), (req, res) => {
 	const {id, plain, toHash} = req.body;
-	let {party} = req.body;
-	if (!party) party = 'TK';
-	logger.debug('write', {id, plain, toHash, party});
+	const {accessToken} = req.locals;
+	let {org} = req.body;
+	logger.debug('write', {id, plain, toHash, org});
 
 	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!party) return errorHandle(`party is ${party}`, res);
+	if (!org) return errorHandle(`org is ${org}`, res);
 	const files = req.files;
 
-	const fileHashs = files.map(({size, path}) => {
-		const startTime = new Date().getTime();
-		const data = fs.readFileSync(path);
-		const hashData = hashAlgo(data);
-		const endTime = new Date().getTime();
-		logger.debug('hashing file', path, {size}, `consumed, ${endTime - startTime}ms`);
-		fs.unlinkSync(path);
-		return hashData;
+	const promise = Promise.resolve();
+
+	const fileStreams = files.map(({path}) => {
+		return fs.createReadStream(path);
 	});
+	const formData = {
+		// Pass a simple key-value pair
+		my_field: 'my_value',
+		// Pass data via Buffers
+		my_buffer: new Buffer([1, 2, 3]),
+		// Pass multiple values /w an Array
+		files: fileStreams,
+	};
+	Request.post({url: 'http://service.com/upload', formData: formData}, (err, resp, body) => {
+		if (err) {
+			return logger.error('upload failed:', err);
+		}
+		logger.debug('Upload successful!  Server responded with:', body);
+	});
+
+
 	const textHash = hashAlgo(toHash);
 
 	const value = `${plain}|${textHash}|${fileHashs.join('|')}`;
 	const {fcn, args, orgName, peerIndex} = {
 		fcn: 'write',
 		args: [id, value],
-		orgName: party,
+		orgName: org,
 		peerIndex: 0,
 	};
 	const {invoke} = require('../app/invoke-chaincode.js');
@@ -68,7 +109,7 @@ router.post('/write', cache.array('files'), (req, res) => {
 	const invalidArgs = invalid.args({args});
 	if (invalidArgs) return errorHandle(invalidArgs, res);
 
-	helper.getOrgAdmin(orgName).then((client) => {
+	promise.then(() => helper.getOrgAdmin(orgName)).then((client) => {
 		const channel = helper.prepareChannel(channelName, client);
 		const peers = helper.newPeers([peerIndex], orgName);
 		return invoke(channel, peers, {
@@ -93,16 +134,15 @@ router.post('/write', cache.array('files'), (req, res) => {
 });
 router.post('/delete', (req, res) => {
 	const {id} = req.body;
-	let {party} = req.body;
-	if (!party) party = 'TK';
-	logger.debug('delete', {id, party});
+	let {org} = req.body;
+	logger.debug('delete', {id, org});
 
 	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!party) return errorHandle(`party is ${party}`, res);
+	if (!org) return errorHandle(`org is ${org}`, res);
 	const {fcn, args, orgName, peerIndex} = {
 		fcn: 'delete',
 		args: [id],
-		orgName: party,
+		orgName: org,
 		peerIndex: 0,
 	};
 	const {invoke} = require('../app/invoke-chaincode.js');
@@ -140,19 +180,25 @@ router.post('/delete', (req, res) => {
 });
 router.post('/read', (req, res) => {
 	const {id} = req.body;
-	let {party} = req.body;
-	if (!party) party = 'TK';
-	logger.debug('read', {id, party});
+	const {org} = req.body;
+	logger.debug('read', {id, org});
 
 	if (!id) return errorHandle(`id is ${id}`, res);
-	if (!party) return errorHandle(`party is ${party}`, res);
+	if (!org) return errorHandle(`org is ${org}`, res);
 
 	const {fcn, args, orgName, peerIndex} = {
 		fcn: 'read',
 		args: [id],
-		orgName: party,
+		orgName: org,
 		peerIndex: 0,
 	};
+	if (org === 'TK') {
+		let {delegatedOrg} = req.body;
+		if (!delegatedOrg) {
+			delegatedOrg = 'TK';
+		}
+		args.push(delegatedOrg);
+	}
 	const {invoke} = require('../app/invoke-chaincode.js');
 
 	logger.debug({chaincodeId, fcn, orgName, peerIndex, channelName});
--- express/oauth2Client.js
+++ express/oauth2Client.js
@@ -11,22 +11,21 @@ const credentials = {
 };
 module.exports.passwordGrant = {
 
-
+	verify:(token)=>{
+	//	TODO
+		return Promise.resolve(token)
+	},
 	getToken: ({username = 'key', password = 'password'}) => {
 		const oauth2 = Oauth2Client.create(credentials);
 		return oauth2.ownerPassword.getToken({username, password})
 			.then((result) => {
-				const accessToken = oauth2.accessToken.create(result);
-				logger.info(accessToken);
-				return Promise.resolve(accessToken);
-			}).catch(err => {
-				logger.error(err);
-				return Promise.reject(err);
+				logger.info(result);
+				return Promise.resolve(result);
 			});
 	},
 	refreshToken: (accessToken) => {
 		const oauth2 = Oauth2Client.create(credentials);
-		const {token: {accessToken: {accessToken:access_token, refreshToken:refresh_token}}} = accessToken;
+		const {accessToken: {accessToken:access_token, refreshToken:refresh_token}} = accessToken;
 		const tokenObject = {
 			access_token,
 			refresh_token,
