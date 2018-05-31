7c33cbae7b321613aaafae4c030b1227af3f1a9f
davidliu <david-khala@hotmail.com>
2018-05-31 14:17:19 +0800

    use curl -S for show error

--- cluster/managerNode/prepare.sh
+++ cluster/managerNode/prepare.sh
@@ -13,21 +13,20 @@ swarmServerIP=$(jq -r ".swarmServer.url" $SWARM_CONFIG)
 swarmServerPort=$(jq ".swarmServer.port" $SWARM_CONFIG)
 swarmBaseUrl=${swarmServerIP}:${swarmServerPort}
 
-if ! curl -s $swarmBaseUrl/; then
+if ! curl -sS $swarmBaseUrl/; then
 	echo no response from swarmServer $swarmBaseUrl
 	exit 1
 fi
 
-CONFIG_JSON=$(curl -s $swarmBaseUrl/config/orgs)
+CONFIG_JSON=$(curl -sS $swarmBaseUrl/config/orgs)
 fabricTag=$(echo $CONFIG_JSON | jq -r ".docker.fabricTag")
 $root/cluster/prepare.sh pull $fabricTag
 thirdPartyTag=$(echo $CONFIG_JSON | jq -r ".docker.thirdPartyTag")
 $root/cluster/prepare.sh pullKafka $thirdPartyTag
 
-leaderInfo=$(curl -s ${swarmBaseUrl}/leader)
-
+leaderInfo=$(curl -sS ${swarmBaseUrl}/leader)
+echo leaderInfo $leaderInfo
 joinToken=$(echo $leaderInfo | jq -r ".managerToken")
-echo joinToken $joinToken
 if ! $joinToken; then
 	echo ... perhaps joined already.
 	$utilsDir/swarm.sh view
@@ -35,6 +34,6 @@ fi
 
 thisHostName=$(hostname)
 thisNodeIP=$($root/common/docker/utils/swarm.sh getNodeIP)
-curl -s -X POST ${swarmBaseUrl}/manager/join -d "{\"ip\":\"${thisNodeIP}\",\"hostname\":\"${thisHostName}\"}" -H "Content-Type: application/json"
+curl -sS -X POST ${swarmBaseUrl}/manager/join -d "{\"ip\":\"${thisNodeIP}\",\"hostname\":\"${thisHostName}\"}" -H "Content-Type: application/json"
 
 $root/cluster/clean.sh
