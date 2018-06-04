1eb243034df73a31c273301cc403ec6445c8500a
davidliu <david-khala@hotmail.com>
2018-06-04 18:38:12 +0800

    back up for signatures reducer

--- cluster/managerNode/signServer.js
+++ cluster/managerNode/signServer.js
@@ -8,7 +8,7 @@ const userUtil = require('../../common/nodejs/user');
 const signUtil = require('../../common/nodejs/multiSign');
 const clientUtil = require('../../common/nodejs/client');
 const pathUtil = require('../../common/nodejs/path');
-const {CryptoPath} = pathUtil;
+const {CryptoPath, homeResolve} = pathUtil;
 const {sha2_256} = require('fabric-client/lib/hash');
 
 const fs = require('fs');
@@ -16,32 +16,38 @@ const fs = require('fs');
  * return two signatures: 1 from orderer 1 from peer
  */
 app.post('/', cache.single('proto'), async (req, res) => {
-	const proto = fs.readFileSync(req.file.path);
+	try {
+		const ordererOrg = 'NewConsensus';
+		const peerOrg = 'NEW';
 
-	logger.info('sign request', 'hash', sha2_256(proto));
+		if (!req.file) throw 'no attachment file';
+		const proto = fs.readFileSync(req.file.path);
+		logger.info('sign request', 'hash', sha2_256(proto));
 
-	const caCryptoConfig = config.MSPROOT;
-	const ordererClient = clientUtil.new();
+		const ordererClient = clientUtil.new();
 
-	const cryptoPath = new CryptoPath(caCryptoConfig, {
-		orderer: {name: 'orderer0', org: 'NewConsensus',},
-		peer: {name: 'newContainer', org: 'NEW'},
-		user: {name: 'Admin'}
-	});
-	let signatures = [];
-	const ordererAdmin = await userUtil.loadFromLocal(cryptoPath, 'orderer', config.orderer.orgs.NewConsensus.MSP.id, ordererClient.getCryptoSuite());
-	await ordererClient.setUserContext(ordererAdmin, true);
+		const cryptoPath = new CryptoPath(homeResolve(config.MSPROOT), {
+			orderer: {name: 'orderer0', org: ordererOrg,},
+			peer: {name: 'newContainer', org: peerOrg},
+			user: {name: 'Admin'}
+		});
+		let signatures = [];
+		const ordererAdmin = await userUtil.loadFromLocal(cryptoPath, 'orderer', config.orderer.orgs[ordererOrg].MSP.id, ordererClient.getCryptoSuite());
+		await ordererClient.setUserContext(ordererAdmin, true);
 
-	const {signatures: ordererAdminSigns} = await signUtil.signs([Promise.resolve(ordererClient)], proto);
-	signatures = signatures.concat(ordererAdminSigns);
-	const peerClient = clientUtil.new();
+		const {signatures: ordererAdminSigns} = await signUtil.signs([Promise.resolve(ordererClient)], proto);
+		signatures = signatures.concat(ordererAdminSigns);
+		const peerClient = clientUtil.new();
 
-	const peerAdmin = await userUtil.loadFromLocal(cryptoPath, 'peer', config.orgs.NEW.MSP.id, peerClient.getCryptoSuite());
-	await peerClient.setUserContext(peerAdmin, true);
-	const {signatures: peerAdminSigns} = await signUtil.signs([Promise.resolve(peerClient)], proto);
-	signatures = signatures.concat(peerAdminSigns);
+		const peerAdmin = await userUtil.loadFromLocal(cryptoPath, 'peer', config.orgs[peerOrg].MSP.id, peerClient.getCryptoSuite());
+		await peerClient.setUserContext(peerAdmin, true);
+		const {signatures: peerAdminSigns} = await signUtil.signs([Promise.resolve(peerClient)], proto);
+		signatures = signatures.concat(peerAdminSigns);
 
-	res.send({signatures});
-
+		res.send({signatures});
+	} catch (err) {
+		logger.error(err);
+		res.status(400).send(err.toString());
+	}
 
 });
