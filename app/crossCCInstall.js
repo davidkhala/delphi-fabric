247aef36d36f9f504be3155fd7d02870e97976ac
davidliu <david-khala@hotmail.com>
2018-10-22 13:30:01 +0800

    longer eventWaitTime for concern chaincode image build time

--- app/crossCCInstall.js
+++ app/crossCCInstall.js
@@ -2,12 +2,15 @@ const {installAll} = require('./installHelper');
 const {instantiate} = require('./instantiateHelper');
 const mainCC = 'mainChain';
 const sideCC = 'sideChain';
+const sideCC2 = 'sideChain2';
 const task = async () => {
 	await installAll(mainCC);
 	await installAll(sideCC);
+	await installAll(sideCC2);
 	const org1 = 'ASTRI.org';
 	const org2 = 'icdd';
 	await instantiate(org1, mainCC);
 	await instantiate(org2, sideCC);
+	await instantiate(org1, sideCC2);
 };
 task();
--- app/crossCCInvoke.js
+++ app/crossCCInvoke.js
@@ -1,6 +1,7 @@
 const {invoke} = require('./invokeHelper');
 const mainCC = 'mainChain';
 const sideCC = 'sideChain';
+const sideCC2 = 'sideChain2';
 const helper = require('./helper');
 const logger = require('../common/nodejs/logger').new('crossCC Invoke');
 const put = async () => {
@@ -15,7 +16,7 @@ const get = async (key) => {
 	const peers = helper.newPeers([0], org2);
 	const fcn = 'get';
 	const args = [key];
-	const response = await invoke(peers, org2, sideCC, fcn, args);
+	const response = await invoke(peers, org2, sideCC2, fcn, args);
 	return response[0];
 };
 const flow = async () => {
