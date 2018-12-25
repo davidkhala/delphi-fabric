dd9fb9f44374169acefaf4a49cb67ef25b5e9fec
davidliu <david-khala@hotmail.com>
2018-12-25 19:41:27 +0800

    complete index

--- cc/cross/crossInvoke.js
+++ cc/cross/crossInvoke.js
@@ -3,15 +3,14 @@ const mainCC = 'mainChain';
 const sideCC = 'sideChain';
 const sideCC2 = 'sideChain2';
 const helper = require('../../app/helper');
-const logger = require('../../common/nodejs/logger').new('crossCC Invoke');
-const put = async () => {
+exports.put = async () => {
 	const org1 = 'ASTRI.org';
 	const peers = helper.newPeers([0], org1);
 	const fcn = 'put';
 	const response = await invoke(peers, org1, sideCC, fcn);
 	return response[0];
 };
-const get = async (key) => {
+exports.get = async (key) => {
 	const org2 = 'icdd';
 	const peers = helper.newPeers([0], org2);
 	const fcn = 'get';
@@ -19,14 +18,4 @@ const get = async (key) => {
 	const response = await invoke(peers, org2, sideCC2, fcn, args);
 	return response[0];
 };
-const flow = async () => {
-	const key = await put();
-	try {
-		const value2 = await get(key);
-		logger.debug({value2});
-	} catch (err) {
-		logger.error(err);
-	}
 
-};
-flow();
--- cc/cross/index.js
+++ cc/cross/index.js
@@ -1,2 +1,15 @@
 const install = require('./crossInstall');
-const {} = require('./crossInvoke');
+const {put, get} = require('./crossInvoke');
+const logger = require('../../common/nodejs/logger').new('crossCC');
+
+const flow = async () => {
+	await install.task();
+	const key = await put();
+	try {
+		const value2 = await get(key);
+		logger.debug({value2});
+	} catch (err) {
+		logger.error(err);
+	}
+};
+flow();
