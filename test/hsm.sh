1488d824ffb46ed6a05e192c5636faf5e7b93ee8
davidLiu <david-khala@hotmail.com>
2019-10-14 14:01:23 +0800

    fixing test/hsm.sh

--- test/hsm.sh
+++ test/hsm.sh
@@ -1,7 +1,10 @@
+set -e
 CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
 projectRoot=$(dirname $CURRENT)
 export SOFTHSM2_CONF=$projectRoot/config/softhsm2.conf
 mkdir -p /tmp/softHSM2/ ## align with softhsm2.conf
 label="fabric"
+echo "cleaning up hsm"
+rm -rf /tmp/softHSM2/*
 $projectRoot/common/bash/softHSM.sh initToken $label
 node $projectRoot/common/nodejs/test/hsmTest.js
