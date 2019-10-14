4e7b493d4af9ad4c8a42a958e9e53191f22e432f
davidLiu <david-khala@hotmail.com>
2019-10-14 15:26:13 +0800

    add non-interactive

--- test/hsm.sh
+++ test/hsm.sh
@@ -6,5 +6,7 @@ mkdir -p /tmp/softHSM2/ ## align with softhsm2.conf
 label="fabric"
 echo "cleaning up hsm"
 rm -rf /tmp/softHSM2/*
+export HSM_SO_PIN="fabric"
+export HSM_PIN="fabric"
 $projectRoot/common/bash/softHSM.sh initToken $label
 node $projectRoot/common/nodejs/test/hsmTest.js
