51e6cecf3e54a945894620e2b09a71f0df363c99
david <david-khala@hotmail.com>
2019-11-19 21:20:51 +0800

    [TODO] commit transaction

--- test/offlineSigningTest.js
+++ test/offlineSigningTest.js
@@ -1,5 +1,6 @@
 const helper = require('../app/helper');
 const channelName = 'allchannel';
+
 const offlineCC = require('../common/nodejs/offline/chaincode');
 const User = require('../common/nodejs/user');
 const Client = require('../common/nodejs/client');
@@ -7,7 +8,6 @@ const task = async () => {
 
 	const orgName = 'astri.org';
 	const client = await helper.getOrgAdmin(orgName);
-	const channel = helper.prepareChannel(channelName, client);
 	const peers = helper.newPeers([0], orgName);
 	const fcn = 'putRaw';
 	const key = 'a';
@@ -18,7 +18,7 @@ const task = async () => {
 	const user = Client.getUser(client);
 	const certificate = User.getCertificate(user);
 
-	const {proposal} = await offlineCC.generateUnsignedProposal(channel, {fcn, args, channelName, chaincodeId}, mspId, certificate);
+	const {proposal} = await offlineCC.unsignedTransactionProposal(channelName, {fcn, args, chaincodeId}, mspId, certificate);
 	const proposalBytes = proposal.toBuffer();
 	const signature = User.sign(user, proposalBytes);
 	const result = await offlineCC.sendSignedProposal(peers, signature, proposalBytes);
