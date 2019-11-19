221d5af567933f4108b022d2a39e00d07ccf6ed2
david <david-khala@hotmail.com>
2019-11-19 14:15:38 +0800

    align operation classify

--- test/operationTest.js
+++ test/operationTest.js
@@ -1,23 +1,28 @@
 const peerUrl = 'https://localhost:9443';
 const ordererUrl = 'https://localhost:8443';
-const operationUtil = require('../common/nodejs/operations');
+const OperationService = require('../common/nodejs/operations');
 const {LoggingLevel} = require('../common/nodejs/remote');
 const logger = require('khala-logger/dev').devLogger('test:operation');
 
 const task = async () => {
-	let logLevel;
 	const helper = require('../app/helper');
+	let peerResult;
 	const peerKey = helper.projectResolve('config/ca-crypto-config/peerOrganizations/astri.org/client/clientKey');
 	const peerCert = helper.projectResolve('config/ca-crypto-config/peerOrganizations/astri.org/client/clientCert');
 	const peerHttpsOptions = {key: peerKey, cert: peerCert, json: true, rejectUnauthorized: false};
-	await operationUtil.setLogLevel(peerUrl, LoggingLevel.warning, peerHttpsOptions);
-	logLevel = await operationUtil.getLogLevel(peerUrl, peerHttpsOptions);
-	console.log(peerUrl, logLevel);
+	const peerService = new OperationService(peerUrl, peerHttpsOptions);
+	await peerService.setLogLevel(LoggingLevel.warning);
+	peerResult = await peerService.getLogLevel();
+	logger.info(peerUrl, peerResult);
 
+	let ordererResult;
 	const ordererKey = helper.projectResolve('config/ca-crypto-config/ordererOrganizations/icdd.astri.org/client/clientKey');
 	const ordererCert = helper.projectResolve('config/ca-crypto-config/ordererOrganizations/icdd.astri.org/client/clientCert');
 	const ordererHttpsOptions = {key: ordererKey, cert: ordererCert, json: true, rejectUnauthorized: false};
-	logLevel = await operationUtil.getLogLevel(ordererUrl, ordererHttpsOptions);
-	console.log(ordererUrl, logLevel);
+	const ordererService = new OperationService(ordererUrl, ordererHttpsOptions);
+	ordererResult = await ordererService.getLogLevel();
+	logger.info(ordererUrl, ordererResult);
+	ordererResult = await ordererService.version();
+	logger.info(ordererUrl, ordererResult);
 };
 task();
