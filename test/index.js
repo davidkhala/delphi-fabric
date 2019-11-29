a1843e2e51f7179640d7d53cdaf5d4b47cbf34bb
david <david-khala@hotmail.com>
2019-11-29 14:24:12 +0800

    throw for node test index

--- test/index.js
+++ test/index.js
@@ -1,4 +1,10 @@
-require('./channelInitTest');
-require('./serviceDiscoveryTest');
-require('./offlineSigningTest');
-require('./fabric-network/index');
+const task = async () => {
+	require('./channelInitTest');
+	require('./serviceDiscoveryTest');
+	require('./offlineSigningTest');
+	require('./fabric-network/index');
+};
+task().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
