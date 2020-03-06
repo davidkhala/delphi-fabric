36613f396dcfd42bf6e4b0a46eae34c7615b9245
david <david-khala@hotmail.com>
2020-03-06 12:51:26 +0800

    back up

--- cc/nodejs/diagnose/install.js
+++ cc/nodejs/diagnose/install.js
@@ -1,17 +1,11 @@
-const {installAll, incrementInstallAll} = require('../../../app/installHelper');
+const {incrementInstallAll} = require('../../../app/installHelper');
 const {instantiate} = require('../../../app/instantiateHelper');
 
 const chaincodeId = 'nodeDiagnose';
 const helper = require('../../../app/helper');
-exports.task = async (step) => {
-	switch (step) {
-		case 0:
-			await installAll(chaincodeId);
-			break;
-		case 1:
-			await incrementInstallAll(chaincodeId);
-			break;
-	}
+exports.task = async () => {
+
+	await incrementInstallAll(chaincodeId);
 
 	const org1 = 'astri.org';
 	const peers = helper.newPeers([0], org1);
