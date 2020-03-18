8badeb3a4b71c90711d5d2197801e36d0b0b0cc6
davidliu <david-khala@hotmail.com>
2020-03-18 18:21:55 +0800

    [DONE] orderer close and reconnect

--- test/ordererTest.js
+++ test/ordererTest.js
@@ -1,14 +1,25 @@
 const helper = require('../app/helper');
+const {projectResolve} = helper;
 const logger = helper.getLogger('test:orderer');
 const OrdererUtil = require('../common/nodejs/builder/orderer');
 const orderers = helper.newOrderers();
-const node_modules = '/home/davidliu/Documents/delphi-fabric/common/nodejs/builder/node_modules';
+const node_modules = projectResolve('common/nodejs/builder/node_modules');
+const OrdererManager = require('../common/nodejs/builder/orderer');
 const task = async () => {
-	const orderer = orderers[1];
+	const orderer = orderers[0];
+	const ordererManager = new OrdererManager(undefined, orderer);
+
 
 	switch (parseInt(process.env.taskID)) {
-		case 0:
-			OrdererUtil.createClient(orderer,node_modules); // TODO WIP
+		case 0: {
+			await ordererManager.ping();
+			ordererManager.close();
+			await ordererManager.reconnect(node_modules);
+			await ordererManager.ping();
+			ordererManager.close();
+			await ordererManager.reconnect();
+			await ordererManager.ping();
+		}
 			break;
 		default: {
 			const result = await OrdererUtil.ping(orderer);
