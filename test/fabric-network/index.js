7c603dcbba0679a56b0541be9f570778ae864b06
david <david-khala@hotmail.com>
2020-04-13 19:39:19 +0800

    [WIP] try fix fabric-network test

--- test/fabric-network/index.js
+++ test/fabric-network/index.js
@@ -1,3 +1,4 @@
+const logger = require('khala-logger/log4js').consoleLogger('test:fabric-network');
 const helper = require('../../app/helper');
 const org = 'astri.org';
 const mspid = 'ASTRIMSP';
@@ -8,10 +9,55 @@ const client = helper.getOrgAdmin(org);
 const Gateway = require('../../common/nodejs/fabric-network/gateway');
 const gateway = new Gateway();
 
-exports.getContract = async (chaincodeId) => {
+const task = async (taskID) => {
+
+	const {chaincodeId} = process.env;
 	const network = await gateway.connect(client, channelName, peer, mspid, orderer);
 	const contract = network.getContract(chaincodeId);
-	return {contract, gateway};
+
+	switch (chaincodeId) {
+		case 'diagnose':
+			switch (parseInt(taskID)) {
+				case 0: {
+
+					let transaction = contract.createTransaction('putRaw');
+					await transaction.submit('key', 'value');
+					await contract.submitTransaction('putRaw', 'key', 'value1');
+					let result;
+					transaction = contract.createTransaction('getRaw');
+					result = await transaction.evaluate('key');
+					logger.info(result.toString());
+					result = await contract.evaluateTransaction('getRaw', 'key');
+					logger.info(result.toString());
+				}
+					break;
+			}
+			break;
+		case 'nodeContracts':
+			let result;
+			switch (parseInt(taskID)) {
+				case 0:
+					await contract.submitTransaction('stress:init');
+					break;
+				case 1:
+					result = await contract.submitTransaction('stress:panic');
+					break;
+				case 2:
+					result = await contract.submitTransaction('panic');
+					break;
+				case 3:
+					result = await contract.submitTransaction('any');
+					break;
+				case 4:
+					result = await contract.submitTransaction('shimError');
+					break;
+			}
+			logger.info(result);
+			break;
+	}
+	gateway.disconnect();
+
 };
+module.exports = task(process.env.taskID);
 
 
--- test/index.js
+++ test/index.js
@@ -1,8 +1,17 @@
-require('./channelInitTest');
-require('./serviceDiscoveryTest');
-require('./fabric-network/diagnose');
-require('./offlineSigningTest');
-process.env.taskID = 0;
-require('./operationTest');
-process.env.taskID = 1;
-require('./operationTest');
+const task = async () => {
+	require('./channelInitTest');
+	require('./serviceDiscoveryTest');
+	{
+		process.env.chaincodeId = 'diagnose';
+		process.env.taskID = 0;
+		await require('./fabric-network');
+	}
+
+	require('./offlineSigningTest');
+	process.env.taskID = 0;
+	require('./operationTest');
+	process.env.taskID = 1;
+	require('./operationTest');
+};
+task();
+
