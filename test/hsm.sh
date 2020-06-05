fa17ca1d3cf906a6b68ec3a787e1532f5ffb8e6c
david <david-khala@hotmail.com>
2020-06-05 11:49:06 +0800

    [WIP] mocha lifecycle install[golang] [WIP] chaincode upgrade[validation upgrade only] [WIP] FIXME fabric-network: Illegal buffer [TODO] discovery [SDK][WIP] Cannot find module 'callsite' [SDK][Eventhub] does not actually support multiple targets fix travis build

---
+++ test/hsm.sh
@@ -0,0 +1,13 @@
+set -e
+CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+projectRoot=$(dirname $CURRENT)
+export SOFTHSM2_CONF=$projectRoot/config/softhsm2.conf
+echo SOFTHSM2_CONF=$SOFTHSM2_CONF
+mkdir -p /tmp/softHSM2/ ## align with softhsm2.conf
+label="fabric"
+echo "cleaning up hsm"
+rm -rf /tmp/softHSM2/*
+export HSM_SO_PIN="fabric"
+export HSM_PIN="fabric"
+$projectRoot/common/bash/softHSM.sh initToken $label
+
