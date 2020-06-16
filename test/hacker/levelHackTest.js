ffbda4a12ed8c7c8be68b6486302757d6f1342dc
david <david-khala@hotmail.com>
2020-06-16 21:40:51 +0800

    lifecycle install[golang] [WIP] chaincode upgrade[validation upgrade only] [SDK][WIP] Cannot find module 'callsite' [SDK][Eventhub] does not actually support multiple targets [SDK] fabric-protos wire problem [TODO] queryInstalled would not change even after removal fix CI

---
+++ test/hacker/levelHackTest.js
@@ -0,0 +1,25 @@
+const {PeerLedger} = require('../../common/nodejs/leveldb');
+const logger = require('khala-logger/log4js').consoleLogger('test:peerLedger');
+const {organizations} = require('../../config/orgs.json');
+const {homeResolve} = require('khala-light-util');
+describe('', () => {
+	const rootPath = homeResolve(organizations.icdd.peers[0].stateVolume);
+	const peerLedger = new PeerLedger(rootPath);
+	logger.debug(peerLedger.statePath.chaincodes());
+	const {stateLeveldb} = peerLedger.statePath.ledgersData;
+	beforeEach(async () => {
+		await stateLeveldb.connect();
+	});
+	afterEach(async () => {
+		await stateLeveldb.disconnect();
+	});
+	it('ledgersData', async function () {
+		this.timeout(30000);
+		const rawList = await stateLeveldb.list();
+		logger.debug('stateLeveldb', rawList.filter(({key, value}) => PeerLedger.filter.stateLeveldb({
+			key,
+			value
+		}, {channel: 'allchannel', chaincodeId: 'diagnose'})));
+	});
+});
+
