67d76a35af3e9a42465dff4a57bc3a8b5f33a82b
DavidLiu <david.yx.liu@oracle.com>
2021-03-24 21:16:12 -0400

    AffiliationServiceBuilder.getAll

--- test/caTest.js
+++ test/caTest.js
@@ -1,37 +1,35 @@
 const helper = require('../app/helper');
 const IdentityService = require('../common/nodejs/builder/identityService');
+const AffiliationService = require('../common/nodejs/builder/affiliationService');
 const CAUtil = require('../common/nodejs/ca');
 const CA = require('../common/nodejs/builder/ca');
 const logger = require('khala-logger/log4js').consoleLogger('test:ca');
 
-const task = async (taskID) => {
+describe('CA Test', function () {
+	this.timeout(3000);
 	const org = 'icdd';
 	const caUrl = 'https://localhost:8054';
 	const admin = helper.getOrgAdminUser(org);
 	const caService = new CA(caUrl).caService;
-
-	let result;
-	switch (taskID) {
-		case 0: {
-			const idService = new IdentityService(caService);
-			const allIDs = await idService.getAll(admin);
-			for (const id of allIDs) {
-				logger.debug(id.id, id.attrs);
-			}
+	it('list id', async () => {
+		const idService = new IdentityService(caService);
+		const allIDs = await idService.getAll(admin);
+		for (const id of allIDs) {
+			logger.debug(id.id, id.attrs);
 		}
+	});
+	it('list Affiliation', async () => {
+		const affiliationService = new AffiliationService(caService);
+		const affiliations = await affiliationService.getAll(admin);
+		logger.info(affiliations);
 
-			break;
-		case 1:
-			result = await CAUtil.intermediateCA.register(caService, admin, {
-				enrollmentID: `${org}.intermediate`,
-				affiliation: org,
-				enrollmentSecret: 'password'
-			});
-			logger.debug(result);
-			break;
-
-	}
-
-
-};
-task(parseInt(process.env.taskID));
+	});
+	it('register', async () => {
+		const result = await CAUtil.intermediateCA.register(caService, admin, {
+			enrollmentID: `${org}.intermediate`,
+			affiliation: org,
+			enrollmentSecret: 'password'
+		});
+		logger.debug(result);
+	});
+});
