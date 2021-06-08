b360ab7930727e3fb486cec9f0c34d622e536195
DavidLiu <david.yx.liu@oracle.com>
2021-06-08 02:14:28 -0400

    [WIP] prune travis

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -e
 CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
-fcn=$1
-remain_params=""
-for ((i = 2; i <= ${#}; i++)); do
-	j=${!i}
-	remain_params="$remain_params $j"
-done
 
 gitSync() {
 	git pull
@@ -17,36 +11,20 @@ updateChaincode() {
 	export GO111MODULE=on
 
 	GOPATH=$(go env GOPATH)
-	mkdir -p $GOPATH/src/github.com/davidkhala/
-	cd $GOPATH/src/github.com/davidkhala/
-	set +e
-	git clone "https://github.com/davidkhala/chaincode.git"
-	set -e
-	cd $GOPATH/src/github.com/davidkhala/chaincode
-	git checkout release-1.4
-	cd $GOPATH/src/github.com/davidkhala/chaincode/golang/master
-	go mod vendor
-	cd -
 
-	cd $GOPATH/src/github.com/davidkhala/chaincode/golang/mainChain
+	curl https://raw.githubusercontent.com/davidkhala/goutils/master/scripts/goCmd.sh | bash -s get github.com/davidkhala/chaincode release-1.4
+
+	cd $GOPATH/src/github.com/davidkhala/chaincode/golang/stress
 	go mod vendor
 	cd -
 
 	cd $GOPATH/src/github.com/davidkhala/chaincode/golang/diagnose
 	go mod vendor
 	cd -
-
-	cd $GOPATH/src/github.com/davidkhala/
-	set +e
-	git clone https://github.com/davidkhala/stupid.git
-	set -e
-	cd $GOPATH/src/github.com/davidkhala/stupid
-	go mod vendor
-	cd -
 }
 
-if [[ -n "$fcn" ]]; then
-	$fcn $remain_params
+if [[ -n "$1" ]]; then
+	"$@"
 else
 	if [[ ! -f "$CURRENT/common/install.sh" ]]; then
 		gitSync
