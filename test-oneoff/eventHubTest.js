a80463abed0f3a99ea4ed6e332031d6ceb46df8a
DavidLiu <david.yx.liu@oracle.com>
2021-07-16 09:37:20 +0800

    [WIP] re-create txid [WIP] block decoder

---
+++ test-oneoff/eventHubTest.js
@@ -0,0 +1,36 @@
+const EventHub = require('../common/nodejs/admin/eventHub');
+const UserUtil = require('../common/nodejs/admin/user');
+const {BlockNumberFilterType: {NEWEST, OLDEST}} = require('../common/nodejs/formatter/eventHub');
+const channelName = 'allchannel';
+const helper = require('../app/helper');
+const org = 'astri.org';
+const logger = require('khala-logger/log4js').consoleLogger('test:eventHub');
+const {header, metadata} = require('../common/nodejs/formatter/blockDecoder');
+describe('eventhub', () => {
+
+	const user = helper.getOrgAdmin(org, 'peer');
+	const channel = helper.prepareChannel(channelName);
+	logger.info(channel.toString());
+	const peer = helper.newPeer(0, org);
+
+	it('configTx', async function () {
+		this.timeout(30000);
+		const eventHub = new EventHub(channel, peer.eventer);
+		const identityContext = UserUtil.getIdentityContext(user);
+		const startBlock = OLDEST;
+		const endBlock = NEWEST;
+		eventHub.build(identityContext, {startBlock, endBlock});
+		await eventHub.connect();
+		const callback = (error, event) => {
+			if (error) {
+				console.error(error);
+				return;
+			}
+			const {block} = event;
+			logger.debug('header', header(block));
+			logger.debug('metadata', metadata(block));
+		};
+		eventHub.blockEvent(callback);
+	});
+});
+
