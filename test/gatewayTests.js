e3b4418e986947e5b82ea11957661d3fd55189c8
DavidLiu <david.yx.liu@oracle.com>
2022-04-14 11:42:51 +0800

    [DONE] @hyperledger/fabric-gateway
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- test/gatewayTests.js
+++ test/gatewayTests.js
@@ -3,17 +3,28 @@ import * as  helper from '../app/helper.js';
 import UserBuilder from '../common/nodejs/admin/user.js';
 import FabricGateway from '../common/nodejs/fabric-gateway/index.js';
 
-describe('build', () => {
-	const user = helper.getOrgAdmin(undefined, 'orderer');
+describe('gateway', () => {
+	const user = helper.getOrgAdmin(undefined, 'peer');
 	const peers = helper.newPeers([0], 'icdd');
 	const peer = peers[0];
-	assert.ok(!!user, 'user not found');
+	assert.ok(user, 'user not found');
 	const userBuilder = new UserBuilder(undefined, user);
 
-	it('gateway', async () => {
-		const gateway = new FabricGateway(peer, userBuilder);
+	const gateway = new FabricGateway(peer, userBuilder);
 
+	it('whoami', async () => {
 		const contract = gateway.getContract('allchannel', 'diagnose');
+		const result = await contract.evaluateTransaction('whoami');
+		console.info(result);
+		gateway.disconnect();
+		await contract.evaluateTransaction('whoami');
 	});
+	it('reconnect', async ()=>{
+
+		gateway.disconnect();
+		gateway.connect()
+		const contract = gateway.getContract('allchannel', 'diagnose');
+		await contract.evaluateTransaction('whoami');
+	})
 
 });
