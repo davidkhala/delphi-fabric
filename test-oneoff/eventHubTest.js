3585a1951045ebcaa689130425b9b183856a743f
david <david-khala@hotmail.com>
2024-01-10 15:32:05 +0800

    before bump to fabric-sdk-node

--- test-oneoff/eventHubTest.js
+++ test-oneoff/eventHubTest.js
@@ -1,14 +1,14 @@
 import EventHub from '../common/nodejs/admin/eventHub.js';
 import UserUtil from '../common/nodejs/admin/user.js';
 import * as helper from '../app/helper.js';
-import {BlockNumberFilterType} from '../common/nodejs/formatter/eventHub';
+import {BlockNumberFilterType} from '../common/nodejs/formatter/eventHub.js';
 import {consoleLogger} from '@davidkhala/logger/log4.js';
 import BlockDecoder from '../common/nodejs/formatter/blockDecoder.js';
 
 const logger = consoleLogger('test:eventHub');
 const {NEWEST, OLDEST} = BlockNumberFilterType;
 const org = 'astri.org';
-describe('eventhub', () => {
+describe('eventhub', function ()  {
 	this.timeout(0);
 	const channelName = 'allchannel';
 	const user = helper.getOrgAdmin(org, 'peer');
@@ -26,6 +26,7 @@ describe('eventhub', () => {
 	it('block parser', async ()=> {
 
 		const eventHub = new EventHub(channel, peer.eventer);
+		console.debug(user)
 		const identityContext = UserUtil.getIdentityContext(user);
 		const startBlock = OLDEST;
 		const endBlock = NEWEST;
